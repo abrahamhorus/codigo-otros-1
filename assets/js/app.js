8764477e75f7ae4535c94ee9251e246f8435c5df
abrahamhorus <110262126+abrahamhorus@users.noreply.github.com>
2023-01-20 19:34:35 -0600

    :C

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,22 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+const $n = document.getElementById('name');
+const $b = document.getElementById('blog');
+const $l = document.getElementById('location');
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
+async function displayUser(username) {
+  $n.textContent = "cargando...";
   const response = await fetch(`${usersEndpoint}/${username}`);
+  $n.textContent = `${data.name}`;
+  $b.textContent = `${data.blog}`;
+  $l.textContent = `${data.location}`;
   console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
 }
 
 function handleError(err) {
   console.log('OH NO!');
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $n.textContent = `Algo salió mal: ${err}`
 }
 
 displayUser('stolinski').catch(handleError);
